370973aa43535c180e0038c7a9532c9e2f207088
Brent Timmermans <brent.timmermans@student.odisee.be>
2017-03-05 11:52:21 +0100

    Added linting

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, ViewEncapsulation } from '@angular/core';
-import { SportsOverviewComponent } from './sports-overview/sports-overview.component';
+// import { SportsOverviewComponent } from './sports-overview/sports-overview.component';
 
 @Component({
 	selector: 'my-app',
