30eea25625c6820639ce97c40d1171db86356999
Brent Timmermans <brent.timmermans@student.odisee.be>
2017-03-10 20:04:36 +0100

    Fix test + deploy to heroku

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,10 +1,11 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 import { SportsOverviewComponent } from './sports-overview/sports-overview.component';
+import { SportItem } from './sport-item/sport-item.component';
 
 describe('App', () => {
 	beforeEach(() => {
-		TestBed.configureTestingModule({ declarations: [AppComponent, SportsOverviewComponent]});
+		TestBed.configureTestingModule({ declarations: [AppComponent, SportsOverviewComponent, SportItem]});
 	});
 	it ('should work', () => {
 		let fixture = TestBed.createComponent(AppComponent);
