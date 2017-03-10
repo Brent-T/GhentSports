8e69556a668b28f8f413848a5bf1faf18976dc38
Brent Timmermans <brent.timmermans@student.odisee.be>
2017-03-10 20:46:55 +0100

    Change package.json for heroku

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+// Serve dist static
+app.use(express.static('dist'));
+
+// Redirect every reques to index.html
+app.get('*', (req, res) => {
+	res.sendFile(path.resolve('dist', 'index.html'));
+});
+
+app.listen(80);
