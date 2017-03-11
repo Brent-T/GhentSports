8688ddcf86841e414aaa215d60643513ca4870f6
Brent-T <brent.timmermans@hotmail.com>
2017-03-11 11:17:30 +0100

    Fixed test - router

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,11 +1,13 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
-import { SportsOverviewComponent } from './sports-overview/sports-overview.component';
-import { SportItem } from './sport-item/sport-item.component';
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('App', () => {
 	beforeEach(() => {
-		TestBed.configureTestingModule({ declarations: [AppComponent, SportsOverviewComponent, SportItem]});
+		TestBed.configureTestingModule({ 
+			declarations: [AppComponent],
+			imports: [RouterTestingModule]
+		});
 	});
 	it ('should work', () => {
 		let fixture = TestBed.createComponent(AppComponent);
