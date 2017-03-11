21cb75be4d5d274d7214f5d3d2f81e8f4c4b5d6b
Brent-T <brent.timmermans@hotmail.com>
2017-03-11 11:03:48 +0100

    Adding sport detail

---
+++ src/app/sport-detail/sport-detail.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+	selector: 'sport-detail',
+	templateUrl: './sport-detail.component.html',
+	styleUrls: ['./sport-detail.component.scss']
+})
+
+export class SportDetail { }
