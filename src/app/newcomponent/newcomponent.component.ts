30c1c7bb51c23d0cb2196a41aadf31969436df8a
Brent-T <brent.timmermans@hotmail.com>
2017-05-12 19:34:35 +0200

    Change sport-icons

--- src/app/newcomponent/newcomponent.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-	selector: 'newcomponent',
-	templateUrl: './newcomponent.component.html',
-	styleUrls: ['./newcomponent.component.scss']
-})
-
-export class NewComponent { }
--- src/services/sports.service.ts
+++ src/services/sports.service.ts
@@ -6,13 +6,13 @@ import { Sport } from './../models/sport';
 @Injectable()
 export class SportsService {
 	private sports: Sport[] = [
-		new Sport(1, 'Football', require('./../assets/icons/football.png'), SportCategory.Football),
-		new Sport(2, 'Basketball', require('./../assets/icons/basketball.png'), SportCategory.Basketball),
-		new Sport(3, 'Running', require('./../assets/icons/running.png'), SportCategory.Running),
-		new Sport(4, 'Pingpong', require('./../assets/icons/pingpong.png'), SportCategory.Pingpong),
-		new Sport(5, 'Fitness', require('./../assets/icons/fitness.png'), SportCategory.Fitness),
-		new Sport(6, 'Skate', require('./../assets/icons/skate.png'), SportCategory.Skate),
-		new Sport(7, 'Volleyball', require('./../assets/icons/volleyball.png'), SportCategory.Volleyball),
+		new Sport(1, 'Football', require('./../assets/sport-icons/football.png'), SportCategory.Football),
+		new Sport(2, 'Basketball', require('./../assets/sport-icons/basketball.png'), SportCategory.Basketball),
+		new Sport(3, 'Running', require('./../assets/sport-icons/running.png'), SportCategory.Running),
+		new Sport(4, 'Pingpong', require('./../assets/sport-icons/pingpong.png'), SportCategory.Pingpong),
+		new Sport(5, 'Fitness', require('./../assets/sport-icons/fitness.png'), SportCategory.Fitness),
+		new Sport(6, 'Skate', require('./../assets/sport-icons/skate.png'), SportCategory.Skate),
+		new Sport(7, 'Volleyball', require('./../assets/sport-icons/volleyball.png'), SportCategory.Volleyball),
 	];;
 
 	constructor() {}
