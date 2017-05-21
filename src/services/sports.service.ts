62f5c8320011800dc4b1c28b7a8203d3ad36eb37
Brent-T <brent.timmermans@hotmail.com>
2017-05-21 22:43:10 +0200

    Relocate assets + fix font bug

--- src/services/sports.service.ts
+++ src/services/sports.service.ts
@@ -6,13 +6,13 @@ import { Sport } from './../models/sport';
 @Injectable()
 export class SportsService {
 	private sports: Sport[] = [
-		new Sport(1, 'Football', require('./../assets/sport-icons/football.png'), SportCategory.Football),
-		new Sport(2, 'Basketball', require('./../assets/sport-icons/basketball.png'), SportCategory.Basketball),
-		new Sport(3, 'Running', require('./../assets/sport-icons/running.png'), SportCategory.Running),
-		new Sport(4, 'Pingpong', require('./../assets/sport-icons/pingpong.png'), SportCategory.Pingpong),
-		new Sport(5, 'Fitness', require('./../assets/sport-icons/fitness.png'), SportCategory.Fitness),
-		new Sport(6, 'Skate', require('./../assets/sport-icons/skate.png'), SportCategory.Skate),
-		new Sport(7, 'Volleyball', require('./../assets/sport-icons/volleyball.png'), SportCategory.Volleyball),
+		new Sport(1, 'Football', require('./../../assets/sport-icons/football.png'), SportCategory.Football),
+		new Sport(2, 'Basketball', require('./../../assets/sport-icons/basketball.png'), SportCategory.Basketball),
+		new Sport(3, 'Running', require('./../../assets/sport-icons/running.png'), SportCategory.Running),
+		new Sport(4, 'Pingpong', require('./../../assets/sport-icons/pingpong.png'), SportCategory.Pingpong),
+		new Sport(5, 'Fitness', require('./../../assets/sport-icons/fitness.png'), SportCategory.Fitness),
+		new Sport(6, 'Skate', require('./../../assets/sport-icons/skate.png'), SportCategory.Skate),
+		new Sport(7, 'Volleyball', require('./../../assets/sport-icons/volleyball.png'), SportCategory.Volleyball),
 	];;
 
 	constructor() {}
