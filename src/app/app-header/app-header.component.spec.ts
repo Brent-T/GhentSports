685af6d90377221e643c42d8badd2d59d2d71e4e
Brent-T <brent.timmermans@hotmail.com>
2017-05-28 21:40:49 +0200

    Testing

--- src/app/app-header/app-header.component.spec.ts
+++ src/app/app-header/app-header.component.spec.ts
@@ -3,46 +3,89 @@ import { By }              from '@angular/platform-browser';
 import { DebugElement }    from '@angular/core';
 
 import { UserService } from './../../services/user.service';
+import { FacebookService, InitParams, LoginResponse, AuthResponse } from 'ngx-facebook';
 
 import { AppHeader } from './app-header.component';
 
-describe('AppHeader', () => {
+describe('AppHeader - title', () => {
 
-  let comp: AppHeader;
-  let fixture: ComponentFixture<AppHeader>;
-  let de: DebugElement;
-  let el: HTMLElement;
+	let comp: AppHeader;
+	let fixture: ComponentFixture<AppHeader>;
+	let de_header_title: DebugElement;
+	let header_title: HTMLElement;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      declarations: [ AppHeader ],
-      providers: [
-	    { provide: ComponentFixtureAutoDetect, useValue: true }
-	  ]
-    });
+	beforeEach(() => {
+		TestBed.configureTestingModule({
+			declarations: [ AppHeader ],
+			providers: [
+				UserService,
+				FacebookService,
+				{ provide: ComponentFixtureAutoDetect, useValue: true }
+			]
+		});
 
-    fixture = TestBed.createComponent(AppHeader); // AppHeader component
+		fixture = TestBed.createComponent(AppHeader); // AppHeader component
 
-    comp = fixture.componentInstance; // AppHeader test instance
+		comp = fixture.componentInstance; // AppHeader test instance
 
-    // query for the title <h1> by CSS element selector
-    de = fixture.debugElement.query(By.css('h1'));
-    el = de.nativeElement;
-  });
+		de_header_title = fixture.debugElement.query(By.css('.header__title'));
+		header_title = de_header_title.nativeElement;
+	});
 
 	it('should display original title', () => {	  
-	  expect(el.textContent).toContain(comp.title);
+		expect(header_title.textContent).toContain(comp.title);
 	});
 
 	it('should still see original title after comp.title change', () => {
-	  const oldTitle = comp.title;
-	  comp.title = 'Test Title';	  
-	  expect(el.textContent).toContain(oldTitle);
+		const oldTitle = comp.title;
+		comp.title = 'Test Title';	  
+		expect(header_title.textContent).toContain(oldTitle);
 	});
 
 	it('should display updated title after detectChanges', () => {
-	  comp.title = 'Test Title';
-	  fixture.detectChanges();
-	  expect(el.textContent).toContain(comp.title);
+		comp.title = 'Test Title';
+		fixture.detectChanges();
+		expect(header_title.textContent).toContain(comp.title);
 	});
 });
+
+// describe('AppHeader - login', () => {
+
+// 	let comp: AppHeader;
+// 	let fixture: ComponentFixture<AppHeader>;
+// 	let userService: UserService;
+// 	let de_header_loggedOut: DebugElement;
+// 	let header_loggedIn: HTMLElement;
+// 	let de_header_loggedIn: DebugElement;
+// 	let header_loggedOut: HTMLElement;
+
+// 	beforeEach(() => {
+// 		TestBed.configureTestingModule({
+// 			declarations: [ AppHeader ],
+// 			providers: [
+// 				UserService,
+// 				FacebookService,
+// 				{ provide: ComponentFixtureAutoDetect, useValue: true }
+// 			]
+// 		});
+
+// 		fixture = TestBed.createComponent(AppHeader); // AppHeader component
+
+// 		comp = fixture.componentInstance; // AppHeader test instance
+// 		userService = fixture.debugElement.injector.get(UserService);
+
+// 		de_header_loggedOut = fixture.debugElement.query(By.css('.header__loggedOut'));
+// 		header_loggedOut = de_header_loggedOut.nativeElement;
+
+// 		de_header_loggedIn = fixture.debugElement.query(By.css('.header__loggedIn'));
+// 		// header_loggedIn = de_header_loggedIn.nativeElement;
+// 	});
+
+// 	it('should show user info', () => {	  
+// 		userService.loginWithFacebook();
+// 	});
+
+// 	it('should log out', () => {	  
+// 		userService.logoutWithFacebook();
+// 	});
+// });
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -2,8 +2,8 @@ import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 import { RouterTestingModule } from '@angular/router/testing';
 
-describe('App', () => {
-	it ('should work', () => {
+describe('Init', () => {
+	it('true is true', () => {
 		expect(true).toBe(true);
 	});
 });
