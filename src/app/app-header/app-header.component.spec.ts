bfdcbbfeebe39e9e891b82b68675bcf1946d4673
Brent-T <brent.timmermans@hotmail.com>
2017-05-29 12:32:26 +0200

    Fixed appheader with test router module

--- src/app/app-header/app-header.component.spec.ts
+++ src/app/app-header/app-header.component.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed, ComponentFixtureAutoDetect } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 import { By }              from '@angular/platform-browser';
 import { DebugElement }    from '@angular/core';
 
@@ -16,7 +17,8 @@ describe('AppHeader - title', () => {
 
 	beforeEach(() => {
 		TestBed.configureTestingModule({
-			declarations: [ AppHeader ],
+			imports: [RouterTestingModule],
+			declarations: [AppHeader],
 			providers: [
 				UserService,
 				FacebookService,
